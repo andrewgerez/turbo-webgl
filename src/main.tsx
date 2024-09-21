f4c0c58e97cb6915479efe0053181e890dd19bd1
Andrew Gerez <andrewgerez18@gmail.com>
2024-09-20 23:37:18 -0300

    refact(200924): pages, components and subcomponents directories

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import App from './app.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
